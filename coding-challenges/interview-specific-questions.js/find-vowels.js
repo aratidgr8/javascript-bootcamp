2ad63d6469f8189d4fbecbe34f66a0aedf4baf79
Arati Bhandare <arati.bhandare@newfold.com>
2022-10-30 20:53:51 +0530

    Code questions

--- coding-challenges/interview-specific-questions.js/find-vowels.js
+++ coding-challenges/interview-specific-questions.js/find-vowels.js
@@ -8,3 +8,13 @@ const findVowels = str => {
     }, 0)
 
 }
+
+/**
+ * Create a string with all vowel characters
+ * Convert given string to lowercase
+ * Split string into characters
+ * Run `reduce` over the split array
+ * Initialize `accumulator` to zero
+ * Increment value of `accumulator` by 1 whenever we find a character in array that matches with any characters in `vowels` string
+ * Return final of value of accumulator, i.e. count of vowels in the string
+ */
